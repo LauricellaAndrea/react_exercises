9d9cbc1bcad1e9d4c9d622e6ce7fce6b2d3ba3e4
LauricellaAndrea <andrealauricella97@gmail.com>
2022-11-02 17:17:55 +0100

    Add-components-04-es

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import { Hello } from "./Hello";
+// import { Hello } from "./Hello";
+import React from "react";
 import ReactDOM from "react-dom";
+import { App } from "./App"; //App conterrò anche le caratteristiche di hello.js
 
-const hello = <Hello />;
+const AppElement = <App/> // Il compomente viene racchiuso in una variabile
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(hello);
+root.render(AppElement, root); // il primo parametro fa riferimento di cosa fare il render, mentre il secondo la locazione di dove farlo
