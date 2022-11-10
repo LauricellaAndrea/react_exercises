983cc62b7d041167dd25211ac99430724a203d34
LauricellaAndrea <andrealauricella97@gmail.com>
2022-11-10 14:33:58 +0100

    modify welcome

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ export class App extends React.Component {
         return (
             <div>
                 <Hello />
-                <Welcome age="25" />
+                <Welcome  />
             </div>
         );
     }
