667622ae2c117ec35f34c11250b13e4eec1786e0
LauricellaAndrea <andrealauricella97@gmail.com>
2022-11-23 23:11:34 +0100

    Add-Todolist

---
+++ src/TodoList.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+export class TodoList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      input: "",
+      items: []
+    }
+  }
+  
+  OnHandleInput = (e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  OnformHandleSubmit = (e) => {
+   e.preventDefault();
+
+   this.setState(state =>({
+    input: '',
+    items: [...this.state.items, <li key={state.items.length }>{state.input}</li>]
+   }))
+    
+  }
+
+  render() {
+
+    return (
+      <React.Fragment>
+  
+       <form>
+        <div>
+       <input type="text" value={this.state.input}
+        placeholder="Add Note" onChange={this.OnHandleInput}/>
+        <button onClick={this.OnformHandleSubmit}>Add items</button>
+        </div>
+        </form>
+        <ul>
+          {this.state.items}
+        </ul>
+      </React.Fragment>
+    )
+  }
+}
