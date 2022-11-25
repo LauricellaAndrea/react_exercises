e2933a2a53b51c7e92c6374a53e6e7e2e25ca684
LauricellaAndrea <andrealauricella97@gmail.com>
2022-11-25 01:11:26 +0100

    add-delete-button-in-new-item

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,54 +2,62 @@ import React from "react";
 
 export class TodoList extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       input: "",
-      items: []
-    }
+      items: [],
+    };
   }
-  
+
   OnHandleInput = (e) => {
     this.setState({
-      input: e.target.value
-    })
-  }
+      input: e.target.value,
+    });
+  };
 
   OnformHandleSubmit = (e) => {
-   e.preventDefault();
+    e.preventDefault();
 
-   this.setState(state =>({
-    input: '',
-    items: [...this.state.items, <li key={state.items.length }>{state.input}</li>]
-   }))
-    
-  }
+    this.setState((state) => ({
+      input: "",
+      items: [
+        <li key={state.items.length}>{state.input}</li>,
+        <button onClick={this.handleRemove} className='remove'>Delete</button>
+      ],
+    }));
+  };
 
-  HandleReset= (e) =>{
+  HandleReset = (e) => {
     this.setState({
-        input: '',
-        items: []
-    })
+      input: "",
+      items: [],
+    });
+  };
 
-  }
+  handleRemove = (event) => {
+    event.target.parentElement.remove()
+}
 
   render() {
-
     return (
       <React.Fragment>
-  
-       <form>
-        <div>
-       <input type="text" value={this.state.input}
-        placeholder="Add Note" onChange={this.OnHandleInput}/>
-        <button onClick={this.OnformHandleSubmit}>Add items</button>
-        <button type="reset" onClick={this.HandleReset}>Reset</button>
-        </div>
+        <form>
+          <div>
+            <input
+              type="text"
+              value={this.state.input}
+              placeholder="Add Note"
+              onChange={this.OnHandleInput}
+            />
+            <button onClick={this.OnformHandleSubmit}>Add items</button>
+            <button type="reset" onClick={this.HandleReset}>
+              Reset
+            </button>
+            <button onClick={this.handleRemove} className='remove'>Delete</button>
+          </div>
         </form>
-        <ul>
-          {this.state.items}
-        </ul>
+        <ul>{this.state.items}</ul>
       </React.Fragment>
-    )
+    );
   }
 }
--- src/app.js
+++ src/app.js
@@ -16,12 +16,12 @@ export class App extends React.Component {
     render(){
         return (
             <div>
-                <Hello />
-                <Welcome age="25" name="John" />
-                <Counter initialValue={10} increment={5} interval={2000}/>
-                <ClickCounter/>
-                <ClickTracker/>
-                <InteractiveWelcome/>
+                {/* <Hello /> */}
+                {/* <Welcome age="25" name="John" /> */}
+                {/* <Counter initialValue={10} increment={5} interval={2000}/> */}
+                {/* <ClickCounter/> */}
+                {/* <ClickTracker/> */}
+                {/* <InteractiveWelcome/> */}
                 <Login/>
                 <UncontrolledLogin/>
                 <TodoList/>
