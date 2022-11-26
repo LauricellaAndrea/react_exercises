c1f9543a2db639afac93acabdd24dcaf319cf943
LauricellaAndrea <andrealauricella97@gmail.com>
2022-11-26 17:50:15 +0100

    Modify container

--- src/Container.js
+++ src/Container.js
@@ -3,6 +3,12 @@ import "./Tailwind.css"
 
 export class Container extends React.Component{
     render(){
-        return (<div className="border-2 border-red-600 border-solid"><h1>Composition Component with Tailwind</h1></div>)
+        return (
+            <div className="border-1 border-solid rounded border-red-800 bg-red-400 text-center">
+            <div className="flex items-center flex-col"><h1 className="text-blue-600">{this.props.title}</h1></div>
+            <div className="flex items-center flex-col">{this.props.children}</div>
+        </div>
+        
+        )
     }
 }
