9a494dd38505c3b27fe9ed2e2adf7934f2526e44
LauricellaAndrea <andrealauricella97@gmail.com>
2022-11-27 18:57:15 +0100

    add context-02

--- src/LanguageContext.js
+++ src/LanguageContext.js
@@ -3,13 +3,24 @@ import { createContext } from "react";
 export const LanguageContext = React.createContext("eng");
 
 export class DisplayLanguage extends React.Component {
+    state = {
+        language: 'en'
+    }
+
+   handleLanguageChange=(e) => {
+    this.setState({
+        language: e.target.value
+    })
+   }
+
   render() {
     return (
       <>
-        <select>
+        <select value={this.state.language} onChange={this.handleLanguageChange}>
           <option lang={this.context}>Italiano</option>
           <option lang={this.context}>English</option>
         </select>
+        <h1>{this.state.Languaget} Change Language</h1>
       </>
     );
   }
