4151c5d770e4fff1dbeda16ae8d2769c7c5ebd48
LauricellaAndrea <andrealauricella97@gmail.com>
2022-12-03 19:50:52 +0100

    Page not found

--- src/AppTwo.js
+++ src/AppTwo.js
@@ -13,14 +13,15 @@ export function AppTwo(){
             <p> 
             <Link to={"/"}>Home</Link>
             <Link to = "/counter">Show Counter</Link>
-            <Link to = "/users/:username">Show GitHubUser </Link>
+            <Link to = "*">Coming soon </Link>
             </p>
         </div>
+
         <Routes>
             <Route path="/" element={<Welcome name={'Johnnie'}/>}/>
-            <Route
-            path="/counter" element={<Counter count={0} interval={1000} amount={5} />} />
-            <Route path="/users/:username" element={<ShowGitHubUser/>}/>
+            <Route path="/counter" element={<Counter count={0} interval={1000} amount={5} />} />
+            <Route path="/users/username" element={<ShowGitHubUser/>}/>
+            <Route path="*" element={<h3>Page not found</h3>}/>
         </Routes>
     
         </BrowserRouter>
